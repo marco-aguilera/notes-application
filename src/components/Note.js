dc54df3ef3e20e129008ca743821c479ec3db9f4
Marco Aguilera <marco.aguilera.guerra@gmail.com>
2022-07-22 23:54:49 -0500

    I added a section for the notes. I also added code to the Node.js

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,13 @@
+import { MdDeleteForever } from 'react-icons/md';
+
 const Note = () => {
-    return <div className='note'></div>;
+    return <div className='note'>
+        <span> Hello! This is our first note! Hurray!</span>
+        <div className='note-foorter'>
+            <small> 12/12/2012 </small>
+            <MdDeleteForever className='delete-icon' size='1.3' />
+        </div>
+    </div>;
 };
 
 export default Note;
